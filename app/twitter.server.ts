02bdb7607130cf7567ff556b8892b7fa75dde966
ntorba <32570754+ntorba@users.noreply.github.com>
2022-09-11 18:10:27 -0400

    Update twitter.server.ts

--- app/twitter.server.ts
+++ app/twitter.server.ts
@@ -146,23 +146,10 @@ export async function getClient(request: Request) {
 
 
 
-interface twitterUser extends users {
-    [key: string]: any;
-    // public_metrics_followers_count: Number
-}
-
-export function flattenTwitterData(data: Array<twitterUser>) {
-    for (const obj of data) {
-        obj.username = obj.username.toLowerCase();
-        obj.public_metrics_followers_count = obj.public_metrics.followers_count;
-        obj.public_metrics_following_count = obj.public_metrics.following_count;
-        obj.public_metrics_tweet_count = obj.public_metrics.tweet_count;
-        obj.public_metrics_listed_count = obj.public_metrics.listed_count;
-        delete obj.public_metrics;
-        delete obj.entities;
-    }
-    return data;
-}
+// interface twitterUser extends users {
+//     [key: string]: any;
+//     // public_metrics_followers_count: Number
+// }
 
 // export function toList(l: ListV2): List {
 //     return {
