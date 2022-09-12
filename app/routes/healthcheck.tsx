7f7efd548b410bbf5ea2fcd34eb842303b152d13
ntorba <32570754+ntorba@users.noreply.github.com>
2022-09-12 14:02:46 -0400

    i should update this with real data later on

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -10,13 +10,8 @@ export async function loader({ request }: LoaderArgs) {
     const url = new URL("/", `http://${host}`);
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
-    await Promise.all([
-      prisma.user.count(),
-      fetch(url.toString(), { method: "HEAD" }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
-    ]);
-    return new Response("OK");
+    console.log(url);
+    return url;
   } catch (error: unknown) {
     console.log("healthcheck ❌", { error });
     return new Response("ERROR", { status: 500 });
