d9109b1dfe05e4ab4f6b451ea31e47aa287c797d
ntorba <32570754+ntorba@users.noreply.github.com>
2022-09-20 10:23:12 -0400

    Update streams.spec.ts

--- app/test/models/streams.spec.ts
+++ app/test/models/streams.spec.ts
@@ -24,7 +24,6 @@ describe("Testing Streams Functions", () => {
     test("Get Stream Tweets", async () => {
         const { stream, seedUsers } = await getStreamByName('new-test');
         console.time("newUpdateStreamTweets")
-        // let tweets = await updateStreamTweetsPromiseAll(api, stream, seedUsers.map((item: any) => (item.user)))
         let tweets = await updateStreamTweets(api, stream, seedUsers.map((item: any) => (item.user)))
         console.timeEnd("newUpdateStreamTweets")
         expect(tweets.length).toBe(4) // promsie for addUsers, addMedia, addTweets, and addTweets for ref tweets
