07108d6b34f12769c57ddb8cfbb283a40acc0474
ntorba <32570754+ntorba@users.noreply.github.com>
2022-09-28 10:52:58 -0400

    file cleanup

--- app/routes/streams/tweets.$tweetId.tsx
+++ app/routes/streams/tweets.$tweetId.tsx
@@ -1,21 +1,10 @@
 import { redirect } from "@remix-run/node";
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
-
-// import { getNoteListItems } from "~/models/note.server";
-// import { requireUserId } from "~/session.server";
-// import { useUser } from "~/utils";
-
 import { getTweet } from "~/models/streams.server";
-
-// type LoaderData = {
-//     // this is a handy way to say: "posts is whatever type getStreams resolves to"
-//     tweet: Awaited<ReturnType<typeof getTweet>>;
-
-// }
 
 export async function loader({ request, params }: LoaderArgs) {
     console.log("in tweets laoder")
@@ -27,10 +16,8 @@ export async function loader({ request, params }: LoaderArgs) {
     return json(data);
 };
 
-export default function StreamsPage() {
+export default function TweetRawDataPage() {
     const data = useLoaderData();
-    console.log("-----")
-    // console.log(relNodes);
     return (
         <div className='overflow-y-auto'>
             <pre>{JSON.stringify(data, null, 2)}</pre>
