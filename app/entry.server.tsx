a98d35203bedb983a788dc7058e9a77e22f60c52
ntorba <32570754+ntorba@users.noreply.github.com>
2022-09-29 12:02:34 -0400

    updating boilerplate to match MUI example
    @andrepology
    need a second pair of eyes on this
    
    these changes were made to match this example: https://github.com/mui/material-ui/tree/master/examples/remix-with-typescript
    
    i'm hoping this is the fix we were looking for for the hydrate issues we were seeing

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -5,49 +5,104 @@ import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
 
+import { renderToString } from 'react-dom/server';
+import createEmotionCache from './src/createEmotionCache';
+import theme from './src/theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import createEmotionServer from '@emotion/server/create-instance';
+
 const ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext
+  remixContext: EntryContext,
 ) {
-  const callbackName = isbot(request.headers.get("user-agent"))
-    ? "onAllReady"
-    : "onShellReady";
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
 
-  return new Promise((resolve, reject) => {
-    let didError = false;
+  const MuiRemixServer = () => (
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <RemixServer context={remixContext} url={request.url} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        [callbackName]: () => {
-          const body = new PassThrough();
+  // Render the component to a string.
+  const html = renderToString(<MuiRemixServer />);
 
-          responseHeaders.set("Content-Type", "text/html");
+  // Grab the CSS from emotion
+  const { styles } = extractCriticalToChunks(html);
 
-          resolve(
-            new Response(body, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            })
-          );
+  let stylesHTML = '';
 
-          pipe(body);
-        },
-        onShellError: (err: unknown) => {
-          reject(err);
-        },
-        onError: (error: unknown) => {
-          didError = true;
+  styles.forEach(({ key, ids, css }) => {
+    const emotionKey = `${key} ${ids.join(' ')}`;
+    const newStyleTag = `<style data-emotion="${emotionKey}">${css}</style>`;
+    stylesHTML = `${stylesHTML}${newStyleTag}`;
+  });
 
-          console.error(error);
-        },
-      }
-    );
+  // Add the Emotion style tags after the insertion point meta tag
+  const markup = html.replace(
+    /<meta(\s)*name="emotion-insertion-point"(\s)*content="emotion-insertion-point"(\s)*\/>/,
+    `<meta name="emotion-insertion-point" content="emotion-insertion-point"/>${stylesHTML}`,
+  );
 
-    setTimeout(abort, ABORT_DELAY);
+  responseHeaders.set('Content-Type', 'text/html');
+
+  return new Response(`<!DOCTYPE html>${markup}`, {
+    status: responseStatusCode,
+    headers: responseHeaders,
   });
 }
+
+// export default function handleRequest(
+//   request: Request,
+//   responseStatusCode: number,
+//   responseHeaders: Headers,
+//   remixContext: EntryContext
+// ) {
+//   const callbackName = isbot(request.headers.get("user-agent"))
+//     ? "onAllReady"
+//     : "onShellReady";
+
+//   return new Promise((resolve, reject) => {
+//     let didError = false;
+
+//     const { pipe, abort } = renderToPipeableStream(
+//       <RemixServer context={remixContext} url={request.url} />,
+//       {
+//         [callbackName]: () => {
+//           const body = new PassThrough();
+
+//           responseHeaders.set("Content-Type", "text/html");
+
+//           resolve(
+//             new Response(body, {
+//               headers: responseHeaders,
+//               status: didError ? 500 : responseStatusCode,
+//             })
+//           );
+
+//           pipe(body);
+//         },
+//         onShellError: (err: unknown) => {
+//           reject(err);
+//         },
+//         onError: (error: unknown) => {
+//           didError = true;
+
+//           console.error(error);
+//         },
+//       }
+//     );
+
+//     setTimeout(abort, ABORT_DELAY);
+//   });
+// }
