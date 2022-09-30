49864bfb937ffb2d914e0c22163b04a870367624
andrepology <andrevacha@gmail.com>
2022-09-30 09:35:24 -0300

    add index modals and update buttons

--- app/src/Layout.tsx
+++ app/src/Layout.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import ProTip from './ProTip';
-import Copyright from './Copyright';
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
+    <Container maxWidth={false} style = {{padding: 0}}>
+      <Box>
         {children}
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
