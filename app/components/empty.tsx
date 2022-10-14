ab72f7e0dd2a66890bee786d08f75530a1be3f66
ntorba <32570754+ntorba@users.noreply.github.com>
2022-10-14 12:55:58 -0400

    adding components from old tweetscape

---
+++ app/components/empty.tsx
@@ -0,0 +1,20 @@
+// import type { ReactNode } from 'react';
+// import cn from 'classnames';
+
+// export interface EmptyProps {
+//   children: ReactNode;
+//   className?: string;
+// }
+
+// export default function Empty({ children, className }: EmptyProps) {
+//   return (
+//     <div
+//       className={cn(
+//         'border rounded text-sm text-gray-400 border-gray-300 dark:text-gray-600 dark:border-gray-700 border-dashed text-center font-normal p-6 flex flex-col items-center justify-center',
+//         className
+//       )}
+//     >
+//       {children}
+//     </div>
+//   );
+// }
