8b638652dd315ad12da77a38d2ca360dbcfed295
ntorba <32570754+ntorba@users.noreply.github.com>
2022-10-26 09:06:03 -0400

    update logout (and auto-logout when api is out of date)

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -1,7 +1,9 @@
-import { redirect } from "@remix-run/node";
-
+import { LoaderArgs, redirect } from "@remix-run/node";
+import { destroySession, getSession } from '~/session.server';
 export { action } from '~/logout.server';
 
-export async function loader() {
-    return redirect("/streams");
+export async function loader({ request }: LoaderArgs) {
+    const session = await getSession(request.headers.get('Cookie'));
+    const headers = { 'Set-Cookie': await destroySession(session) };
+    return redirect("/streams", { headers });
 }
