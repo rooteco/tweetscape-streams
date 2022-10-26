bcb9b6b9f51ff4d3ad343f13e047d510ff017dbe
ntorba <32570754+ntorba@users.noreply.github.com>
2022-10-26 15:41:53 -0400

    comment out the X section for now
    not allowing for hidden topics yet

--- app/components/ContextAnnotationChip.tsx
+++ app/components/ContextAnnotationChip.tsx
@@ -33,7 +33,7 @@ function ContextAnnotationChip({ keyValue, value, caEntities, hideTopics, stream
                 <button className="" type="submit" name="topicFilter" value={keyValue}>
                     {`${keyValue} ${value ? ', ' + value : ''}`}
                 </button>
-                <button
+                {/* <button
                     type="submit"
                     name="hideTopic"
                     value={keyValue}
@@ -45,7 +45,7 @@ function ContextAnnotationChip({ keyValue, value, caEntities, hideTopics, stream
                             d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z">
                         </path>
                     </svg>
-                </button>
+                </button> */}
             </span>
         </Form>
     )
