7b8289f4517a7d111dea8c0512d8f046b4344f92
ntorba <32570754+ntorba@users.noreply.github.com>
2022-10-28 12:23:57 -0400

    Update users.$username.tsx

--- app/routes/streams/users.$username.tsx
+++ app/routes/streams/users.$username.tsx
@@ -9,6 +9,8 @@ import { getMetaFollowers, getUserByUsernameDB, getUserIndexedTweets } from "~/m
 import { getClient } from "~/twitter.server";
 import Tweet from '~/components/Tweet';
 import { indexUser } from "~/models/user.server";
+import CompactProfile from '~/components/CompactProfile';
+
 
 
 
@@ -36,18 +38,44 @@ export default function TweetRawDataPage() {
         <div className='h-full overflow-y-auto'>
             <div className="flex flex-row flex-nowrap items-top gap-5 justify-center py-12">
                 <div className="just-a-border h-full p-6 text-left flex-1 overflow-x-auto">
-                    <div className="mb-2 text-sm font-medium uppercase">Account Info</div>
+                    {/* <!-- Profile Card! --> */}
+                    <div className="border-2 mb-2 rounded">
+                        <div className="flex">
+                            <h2 className="text-xl mr-4 my-auto leading-6 font-bold text-gray-600">{data.user.properties.name}</h2>
+                            <p className="text-sm mr-4 my-auto leading-5 font-medium text-gray-600">{data.user.properties.username}</p>
+                            <span
+                                className={`bg-purple-200 ml-auto m-2 p-2 rounded-full text-gray-900  font-semibold text-sm ursor-pointer active:bg-gray-300 transition duration-300 ease`}
+                            >
+                                Person
+                            </span>
+                        </div>
+                        <div className="flex items-center">
+                            <img
+                                style={{ height: "9rem", width: "9rem" }}
+                                className="md rounded-full relative border-4 border-gray-900"
+                                src={data.user.properties.profile_image_url.split("_normal")[0] + "_400x400." + data.user.properties.profile_image_url.split(".").slice(-1)[0]} alt=""
+                            />
+                            <p
+                                className="text-gray-400 text-xl p-6 leading-tight"
+                            >
+                                {data.user.properties.description}
+                            </p>
+                        </div>
+                        <hr />
+                        <div className="flex">
+                            <div className="text-center pr-3"><span className="font-bold text-gray-400">{data.user.properties["public_metrics.following_count"]}</span><span className="text-gray-600"> Following</span></div>
+                            <div className="text-center px-3"><span className="font-bold text-gray-400">{data.user.properties["public_metrics.followers_count"]}</span><span className="text-gray-600"> Followers</span></div>
+                        </div>
+                    </div>
                     <pre>{JSON.stringify(data.user, null, 2)}</pre>
                 </div>
                 <div className="just-a-border h-full p-6 text-left flex-1 flex-grow">
                     <h1 className="mb-2 text-sm font-medium uppercase">
                         You and {params.username} both follow these accounts:
                     </h1>
-                    <pre>
-                        {
-                            data.metaFollowers.map((row) => (<p>{row.properties.username} - {row.properties["public_metrics.followers_count"]}</p>))
-                        }
-                    </pre>
+                    {data.metaFollowers.map((user: userNode) => (
+                        <CompactProfile user={user} key={user.elementId} streamName={"fakeness for now"} />
+                    ))}
                 </div>
                 <div className="just-a-border h-full p-6 text-left flex-1 flex-grow">
                     <h1 className="mb-2 text-sm font-medium uppercase">
