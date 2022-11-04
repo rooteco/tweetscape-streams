66a1ba12b1d11361f7ad3d2303d74cf3d9bf51e9
ntorba <32570754+ntorba@users.noreply.github.com>
2022-11-04 16:08:29 -0400

    remove sidebar from page if not logged in

--- app/routes/streams.tsx
+++ app/routes/streams.tsx
@@ -130,9 +130,6 @@ export const loader: LoaderFunction = async ({ request }: LoaderArgs) => {
                 });
                 log.info(`Setting session uid (${user.id}) for ${context}...`);
                 session.set('uid', user.id.toString());
-                // userLists = await getUserTwitterLists(api, user);
-                // let owned = await addUserOwnedLists(user, userLists.ownedLists)
-                // let followed = await addUserFollowedLists(user, userLists.followedLists)
             }
         }
     } catch (e) {
@@ -186,28 +183,32 @@ export default function StreamsPage() {
             <div className="bg-gradient-to-r from-gray-50 via-white to-white grow px-4 py-2 lg:max-w-2xl 2xl:max-w-full  z-10">
                 <Outlet />
             </div>
+            {
+                user ?
+                    <div className="flex flex-col border-r space-y-16 w-96 max-w-96 pr-4 pb-6 pl-6">
+                        <div className="relative flex flex-row space-x-2 w-full ml-2 mt-4">
+                            {/* Either 'Create A Stream and Login/Logout' or 'Export Stream or Delete Stream' */}
+                            {streamsRoot ? <CreateAndLogin user={user} /> : <ExportAndDelete user={user} />}
 
-            <div className="flex flex-col border-r space-y-16 w-96 max-w-96 pr-4 pb-6 pl-6">
-                <div className="relative flex flex-row space-x-2 w-full ml-2 mt-4">
-                    {/* Either 'Create A Stream and Login/Logout' or 'Export Stream or Delete Stream' */}
-                    {streamsRoot ? <CreateAndLogin user={user} /> : <ExportAndDelete user={user} />}
+                            <div className="absolute right-6 top-12 flex flex-col items-end space-y-6 z-0">
+                                <p className="text-lg font-bold justify-center align-middle text-gray-100/50" style={{ fontSize: 64 }}>Stream</p>
+                                <p className="text-lg font-bold justify-center align-middle  text-gray-100/50" style={{ fontSize: 64 }}>Seeding</p>
+                            </div>
+                        </div>
 
-                    <div className="absolute right-6 top-12 flex flex-col items-end space-y-6 z-0">
-                        <p className="text-lg font-bold justify-center align-middle text-gray-100/50" style={{ fontSize: 64 }}>Stream</p>
-                        <p className="text-lg font-bold justify-center align-middle  text-gray-100/50" style={{ fontSize: 64 }}>Seeding</p>
+                        {/* List of Streams */}
+                        <div className="flex flex-col space-y-0.5 flex-1 z-10">
+                            <p className="ml-2 text-slate-400 font-medium text-xs"> {user ? `@${user.username}'s` : "Public"} Streams </p>
+                            <StreamAccordion streams={userStreams} lists={lists} />
+                        </div>
+                        <div className="flex flex-col space-y-0.5 flex-1 z-10">
+                            <p className="ml-2 text-slate-400 font-medium text-xs"> Public Streams (Created By Other Users) </p>
+                            <StreamAccordion streams={allStreams} lists={lists} />
+                        </div>
                     </div>
-                </div>
-
-                {/* List of Streams */}
-                <div className="flex flex-col space-y-0.5 flex-1 z-10">
-                    <p className="ml-2 text-slate-400 font-medium text-xs"> {user ? `@${user.username}'s` : "Public"} Streams </p>
-                    <StreamAccordion streams={userStreams} lists={lists} />
-                </div>
-                <div className="flex flex-col space-y-0.5 flex-1 z-10">
-                    <p className="ml-2 text-slate-400 font-medium text-xs"> Public Streams (Created By Other Users) </p>
-                    <StreamAccordion streams={allStreams} lists={lists} />
-                </div>
-            </div>
+                    :
+                    null
+            }
         </div>
     );
 }
