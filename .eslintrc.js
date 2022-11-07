9e62bdc1ab176c501bf80e7f1d1b52e2997f76a2
ntorba <32570754+ntorba@users.noreply.github.com>
2022-11-07 14:15:09 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,5 @@ module.exports = {
       version: 28,
     },
   },
-  ignorePatterns: ["node_modules/", "dist/", "build/", "coverage/", "postgres-data/"],
+  ignorePatterns: ["node_modules/", "dist/", "build/", "coverage/", "postgres-data/", "neo4j"],
 };
